9ac8106dd0dada6511d1cb99e14566b858bc01ee
nguyenquanghuy1999 <khongcanemail896@gmail.com>
2019-10-17 11:01:33 +0700

    thuộc tính

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo, {ReactComponent} from './logo.svg';
 import './App.css';
+import SinhVien from './compoments/SinhVien/SinhVien';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render(){
+    return(
+    
+      <div className = "App">
+        <h1>Ứng dụng React Đầu Tiên</h1>
+        <p>Nguyễn Quang Huy</p>
+        <SinhVien hoten ="nguyễn như quỳnh" tuoi ="20">
+          Sở thích : chơi thể thao, xem phim, nghe nhạc.....
+        </SinhVien>
+        <SinhVien hoten = "nguyễn kim anh" tuoi ="19"/>
+
+        <SinhVien/> 
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/compoments/SinhVien/SinhVien.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const intuoi = () => {
+    return Math.floor(Math.random()* 100);
+}
+
+
+
+const sinhVien = (props) => {
+    return(
+        <div>
+             <p>{ props.hoten } -tuổi: { props.tuoi} </p>
+             <p>{ props.children}</p>
+        </div>
+    );
+   
+}
+
+export default sinhVien;
